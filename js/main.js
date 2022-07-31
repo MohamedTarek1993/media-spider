90a7bb22c61d469c6bfcae34b5633d361bc4f941
MohamedTarek1993 <www.mtarek789@gmail.com>
2022-07-31 13:23:13 +0200

    31/7

--- js/main.js
+++ js/main.js
@@ -31,3 +31,13 @@
 // animated hamburger icon
 
 
+//=========== Active navbar Link in multi pages =============
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll(".navbar-nav .nav-item .nav-link");
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+  if (menuItem[i].href === currentLocation) {
+    menuItem[i].className = "nav-link active";
+  }
+}
+//=========== Active navbar Link in multi pages =============
